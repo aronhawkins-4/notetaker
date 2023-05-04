75a12e323a753a42d6b7e07dde463ca9b2353136
aronhawkins-4 <ahawki18@asu.edu>
2023-05-04 00:48:58 -0500

    using Nextjs Image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,34 @@
-import { signIn, signOut, useSession } from 'next-auth/react'
+import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
 
 export const Header = () => {
-  const { data: sessionData } = useSession()
+  const { data: sessionData } = useSession();
 
   return (
-    <div className='navbar bg-primary text-primary-content'>
-      <div className='flex-1 pl-5 text-3xl font-bold'>
-        {sessionData?.user?.name ? `Notes for ${sessionData.user.name}` : ''}
+    <div className="navbar bg-primary text-primary-content">
+      <div className="flex-1 pl-5 text-3xl font-bold">
+        {sessionData?.user?.name ? `Notes for ${sessionData.user.name}` : ""}
       </div>
-      <div className='flex-none gap-2'>
-        <div className='dropdown-end dropdown'>
+      <div className="flex-none gap-2">
+        <div className="dropdown-end dropdown">
           {sessionData?.user ? (
             <label
               tabIndex={0}
-              className='btn-ghost btn-circle avatar btn'
-              onClick={() => void signOut}
+              className="btn-ghost btn-circle avatar btn"
+              onClick={() => void signOut()}
             >
-              <div className='w-10 rounded-full'>
-                <img
-                  src={sessionData?.user?.image ?? ''}
-                  alt={`${sessionData?.user?.name}'s profile picture` ?? ''}
+              <div className="w-10 rounded-full">
+                <Image
+                  src={sessionData?.user?.image ?? ""}
+                  alt={sessionData?.user?.name ?? ""}
+                  width={32}
+                  height={32}
                 />
               </div>
             </label>
           ) : (
             <button
-              className='btn-ghost rounded-btn btn'
+              className="btn-ghost rounded-btn btn"
               onClick={() => void signIn()}
             >
               Sign in
@@ -34,5 +37,5 @@ export const Header = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
